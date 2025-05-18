f9082db91cc720a8635b0d6934782f8a7c9adbac
MuhammadAmin02 <muhammadamininto011@gmail.com>
2025-05-18 14:11:21 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let a = prompt("Ismingizni kiriting")
+let b = prompt('Bironta harf kiriting')
+console.log(a.includes('i'));
+
+
+if (a.includes(b)) {
+    alert('ismingizda bu harf bor ')
+}
+
+else {
+
+    alert('ismingizda bu harf yoq ')
+}
